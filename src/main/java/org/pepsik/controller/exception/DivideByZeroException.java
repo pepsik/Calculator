76cb0192759d4443249caf7e4f829e07f65c291d
MAlex <alexmaximovj@gmail.com>
2016-08-19 19:57:51 +0300

    missed

---
+++ src/main/java/org/pepsik/controller/exception/DivideByZeroException.java
@@ -0,0 +1,10 @@
+package org.pepsik.controller.exception;
+
+/**
+ * Created by admin on 8/19/2016.
+ */
+public class DivideByZeroException extends Exception {
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+}
